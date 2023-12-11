8f9ac0d9af2c1189bcb0b443f3c6c43d66632e1e
nakkim <kdkeiie8@gmail.com>
2023-12-11 23:42:20 +0900

    chore: typeorm 이용하여 db 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5001,
+      username: 'nakkim',
+      password: 'nakkim@test',
+      database: 'booking_dev',
+      entities: [__dirname + '/entity/*.entity{.ts,.js}'],
+      logging: true,
+      synchronize: false,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
