21b4f1f39d638e3c340349704b1d8ade77a8dcce
nakkim <kdkeiie8@gmail.com>
2023-12-11 23:50:49 +0900

    feat: ValidationPipe 설정

--- src/seller/dto/create-seller.dto.ts
+++ src/seller/dto/create-seller.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateSellerDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Length(1, 50)
   name: string;
 }
