031a912e392154227176bf27da476bd9ed4edf79
nakkim <kdkeiie8@gmail.com>
2023-12-13 22:06:50 +0900

    feat: 판매자 휴무 수정시 예약 확인 후 예외처리

--- src/seller/seller.service.ts
+++ src/seller/seller.service.ts
@@ -1,4 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
+import { In, MoreThan } from 'typeorm';
 import { BookingRepository } from '../booking/booking.repository';
 import { BookingService } from '../booking/booking.service';
 import { BookingStatus } from '../entity/booking.entity';
@@ -37,13 +39,46 @@ export class SellerService {
     return user;
   }
 
+  /**
+   * 휴무로 지정한 날에
+   * - 대기중인 예약이 있을 경우 취소 처리됨
+   * - 확정된 예약이 있을 경우 휴무 지정 불가
+   * @param offDate YYYY-MM-DD 형식의 날짜 배열
+   * @param offDay MON, TUE, WED, THU, FRI, SAT, SUN 형식의 요일 배열
+   */
   async updateOff(userId: number, offDate?: string[], offDay?: string[]) {
+    if (offDate?.length) {
+      const today = dayjs().format('YYYY-MM-DD');
+      const isAfterToday = offDate.every((date) => dayjs(date).isAfter(today));
+      if (!isAfterToday) {
+        throw new BadRequestException('휴무일은 오늘 이후로만 지정 가능합니다.');
+      }
+    }
+
     const user = await this.sellerRepository.findOrThrow(userId);
-    const off = Object.assign({}, { ...(offDate?.length && { offDate }), ...(offDay?.length && { offDay }) });
+    Object.assign(user, { ...(offDate?.length && { offDate }), ...(offDay?.length && { offDay }) });
 
-    await this.sellerRepository.update({ id: userId }, off);
+    const bookings = await this.bookingRepository.findBy({
+      sellerId: userId,
+      date: MoreThan(dayjs().format('YYYY-MM-DD')),
+      status: In([BookingStatus.PENDING, BookingStatus.APPROVED]),
+    });
 
-    return { ...user, ...off };
+    const approvedBookings = bookings.filter((b) => b.status === BookingStatus.APPROVED);
+    if (approvedBookings.some((b) => user.isOff(b.date))) {
+      throw new BadRequestException('확정된 예약이 있는 날은 휴무일로 지정할 수 없습니다.');
+    }
+
+    const pendingBookings = bookings.filter((b) => b.status === BookingStatus.PENDING);
+    pendingBookings.forEach((b) => {
+      if (user.isOff(b.date)) {
+        this.bookingService.updateBookingStatus(b, BookingStatus.REJECTED);
+      }
+    });
+
+    await this.sellerRepository.update({ id: userId }, user);
+
+    return user;
   }
 
   async updateBookingStatus(sellerId: number, bookingId: string, status: BookingStatus) {
